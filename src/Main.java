d4b3da913c5c173af595fa986262aab77c8ed72d
20253ds126-sketch <20253ds126@utez.edu.mx>
2026-01-19 20:22:55 -0600

    Refactorizacion con separacion de responsabilidades

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,50 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final int TOTAL_NUMEROS = 3;
-
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        int a = leerEntero(scanner, "a");
-        int b = leerEntero(scanner, "b");
-        int c = leerEntero(scanner, "c");
+        int a = pedirEntero(scanner, "a: ");
+        int b = pedirEntero(scanner, "b: ");
+        int c = pedirEntero(scanner, "c: ");
 
         int suma = calcularSuma(a, b, c);
-        double promedio = calcularPromedio(suma, TOTAL_NUMEROS);
 
-        mostrarResultados(suma, promedio);
+        System.out.println("suma=" + suma);
+        System.out.println("prom=" + calcularPromedio(suma));
 
         scanner.close();
     }
 
-    private static int leerEntero(Scanner scanner, String nombreVariable) {
-        System.out.print(nombreVariable + ": ");
+    /**
+     * Metodo que retorna un entero capturado por consola
+     * @param scanner Scanner previamente creado en main
+     * @param mensaje Mensaje que se muestra al usuario
+     * @return valor entero ingresado
+     */
+    public static int pedirEntero(Scanner scanner, String mensaje) {
+        System.out.print(mensaje);
         return scanner.nextInt();
     }
 
-    private static int calcularSuma(int a, int b, int c) {
+    /**
+     * Metodo que calcula la suma de tres numeros
+     * @param a primer valor
+     * @param b segundo valor
+     * @param c tercer valor
+     * @return suma de los valores
+     */
+    public static int calcularSuma(int a, int b, int c) {
         return a + b + c;
     }
 
-    private static double calcularPromedio(int suma, int total) {
-        return suma / (double) total;
-    }
-
-    private static void mostrarResultados(int suma, double promedio) {
-        System.out.println("suma = " + suma);
-        System.out.println("prom = " + promedio);
+    /**
+     * Metodo que calcula el promedio de la suma
+     * @param suma suma total de los valores
+     * @return promedio
+     */
+    public static double calcularPromedio(int suma) {
+        return suma / 3.0;
     }
 }
-
-
